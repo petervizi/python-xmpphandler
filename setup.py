73d6b08ef43d9d3f9dc7a32b717f6fa6e67b238e
Peter Vizi <peter.vizi@gmail.com>
2009-01-19 20:37:25 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name="Python XMPPHandler", 
+      version="0.1",
+      author="Peter Vizi",
+      author_email="peter.vizi@gmail.com",
+      description="A subclass of logging.handlers.Handler that uses XMPP protocol",
+      license="GPLv3",
+      packages=['xmpphandler'])
---
+++ xmpphandler/__init__.py
@@ -0,0 +1,6 @@
+# -*- codign: utf-8 -*-
+
+__authors__ = "Peter Vizi"
+__license__ = "GPLv3"
+__version__ = "0.1"
+__docformat__ = "restructuredtext en"
